8a8976938c176fee2d46be2e9b9073241e06fdc5
PolGibert8 <pgibertm@edu.tecnocampus.cat>
2023-05-09 18:33:11 +0200

    Bullet speed

---
+++ Assets/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float Speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        SetVelocity();
+    }
+
+    void SetVelocity()
+    {
+        _rigidbody.velocity = transform.right * Speed;
+    }
+
+   
+
+    private void OnCollisionEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
