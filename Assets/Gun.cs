d8f4b7e1a5843e60f747d96a7336491672e7e5e4
PolGibert8 <pgibertm@edu.tecnocampus.cat>
2023-05-09 18:17:07 +0200

    Bullet implementation

---
+++ Assets/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Weapon
+{
+    public GameObject BulletPrefab;
+    public Transform FirePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldShoot())
+        {
+            Fire();
+        }
+    }
+
+    public override void Fire()
+    {
+        Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+
+    bool ShouldShoot()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+}
