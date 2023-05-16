6ed5bb3cdcc8524c218bdd9fe2aed80b561a8001
GabrielC22 <132926523+GabrielC22@users.noreply.github.com>
2023-05-16 18:13:41 +0200

    Health system finished

---
+++ Assets/Scripts/KillObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillObject : MonoBehaviour
+{
+    private HealthSystem health;
+
+    void Awake()
+    {
+        health = GetComponent<HealthSystem>();    
+    }
+
+    private void OnEnable()
+    {
+        health.OnDeath += OnDeath;
+    }
+
+    private void OnDisable()
+    {
+        health.OnDeath -= OnDeath;
+    }
+
+    private void OnDeath()
+    {
+        Debug.Log($"{gameObject.name} death!");
+        Destroy(gameObject);
+    }
+}
